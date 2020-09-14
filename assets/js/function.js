59a38391721f7b2e18181e2e9918560fbeb4cd30
HenniGenius <henni.genius@gmail.com>
2020-09-14 20:22:23 +0200

    Black Design

--- assets/js/function.js
+++ assets/js/function.js
@@ -11,7 +11,7 @@ async function loadUser(id) {/*
 		"_type": "user",
 		"about_me": {},
 		"about_me_summary": "Manche gehen, manche bleiben, dankbar bin ich beiden\n\n\n\nGlück ist harte Arbeit und kein Wunder\n\n\n\nHerkunft: Frankfurt\nGenius Start: 6. März 2019\nAktiv auf Genius seit: Januar 2020\nFortschritte:\nIQ:\n02. Mai 2020: 1.000 IQ\n01. Juni 2020: 5.000 IQ\n11. Juni 2020: 10.000 IQ\n27. Juni 2020: 15.000 IQ\n06. Juli 2020: 20.000 IQ\n27. Juli 2020: 25.000 IQ\n\nAndere Fortschritte:\n25. Juni 2020: 187 Transcriptions xD\n06. Juli 2020: Erste Transcription über 40k Views\n28. Juli 2020: 400 Transcriptions\n\n\n\nAbzeichen:\n\n\n\n\n\n\n\n\n\n\n\n Created with Genius Graph\nDanke an @GeniusMoblin für den Banner\nDanke an @Crackar für das Coaching\nMein Twitter",
-		"annotations_count": 174,
+		"annotations_count": 204,
 		"api_path": "/users/8068117",
 		"avatar": {
 		  "tiny": {
@@ -45,16 +45,16 @@ async function loadUser(id) {/*
 		},
 		"channel_iqs": [],
 		"custom_header_image_url": "https://images.genius.com/32ed9250e32e40f41ed4fdea03b510d2.1000x165x1.png",
-		"followed_users_count": 12,
-		"followers_count": 12,
+		"followed_users_count": 14,
+		"followers_count": 14,
 		"header_image_url": "https://images.genius.com/32ed9250e32e40f41ed4fdea03b510d2.1000x165x1.png",
 		"human_readable_role_for_display": "Contributor",
 		"id": 8068117,
-		"iq": 25659,
+		"iq": 36123,
 		"iq_for_display": "25,659",
 		"is_meme_verified": false,
 		"is_verified": false,
-		"login": "Kontrollieren",
+		"login": "Henni13",
 		"name": "Henni",
 		"photo_url": "https://images.genius.com/1e3e426783719ce6d81e196030f245fb.1000x1000x1.jpg",
 		"pusher_channel": "private-user-8068117",
@@ -63,9 +63,9 @@ async function loadUser(id) {/*
 		  "contributor"
 		],
 		"tracking_paths": {
-		  "aggregate": "/Kontrollieren"
+		  "aggregate": "/Henni13"
 		},
-		"url": "https://genius.com/Kontrollieren",
+		"url": "https://genius.com/Henni13",
 		"current_user_metadata": {
 		  "permissions": [
 			"follow",
@@ -114,16 +114,16 @@ async function loadUser(id) {/*
 		  "name": "Henni",
 		  "slug": "Henni",
 		  "url": "https://genius.com/artists/Henni",
-		  "iq": 25665
+		  "iq": 36123
 		},
 		"stats": {
-		  "annotations_count": 178,
-		  "answers_count": 16,
-		  "comments_count": 47,
+		  "annotations_count": 204,
+		  "answers_count": 17,
+		  "comments_count": 56,
 		  "forum_posts_count": 6,
-		  "pyongs_count": 18,
-		  "questions_count": 11,
-		  "transcriptions_count": 401
+		  "pyongs_count": 23,
+		  "questions_count": 12,
+		  "transcriptions_count": 555
 		}
 	  }
 }
